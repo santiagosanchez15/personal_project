f1922be2c9996705db23f7428509a37ea4aa2608
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-07-12 13:58:29 -0700

    new comments

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from parse_api_dic import api_to_data
 from compiled_data_into_pandas import data_to_dataframe
 
 def main():
-    url : str = "https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/food-vendors/records"
+    url : str = "https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/food-vendors/records" # thats the given url but any otherone may work
     fetch_limit: dict[str,int] = {"limit": 5}
-    data = api_to_data(url, fetch_limit)
-    dataframe = data_to_dataframe(data)
+    data = api_to_data(url, fetch_limit) #get tdata in json format
+    dataframe = data_to_dataframe(data) #get data in json_normalizer format, tabulated
     print(dataframe)
 
 main()
